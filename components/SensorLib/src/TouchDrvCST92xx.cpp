9adbcf6d5452ceba914cb56e677195f775291ac4
Carlos Almeida <carlos.eduardo.4lmeida@gmail.com>
2026-06-19 22:59:18 +0100

    removed anoying log

--- components/SensorLib/src/TouchDrvCST92xx.cpp
+++ components/SensorLib/src/TouchDrvCST92xx.cpp
@@ -105,7 +105,7 @@ uint8_t TouchDrvCST92xx::getPoint(int16_t *x_array, int16_t *y_array, uint8_t ge
     write_buffer[1] = lowByte(CST92XX_READ_COMMAND);
     res = writeThenRead(write_buffer, 2, read_buffer, sizeof(read_buffer));
     if (res != DEV_WIRE_NONE) {
-        log_e("Write read command error");
+        //log_e("Write read command error");
         return 0;
     }
     // Write read ack
@@ -114,7 +114,7 @@ uint8_t TouchDrvCST92xx::getPoint(int16_t *x_array, int16_t *y_array, uint8_t ge
     write_buffer[2] = CST92XX_ACK;
     res = writeBuffer(write_buffer, 3);
     if (res != DEV_WIRE_NONE) {
-        log_e("Write read ack error");
+        //log_e("Write read ack error");
         return 0;
     }
 

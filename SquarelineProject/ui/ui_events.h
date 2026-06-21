a739ca65f75e3c49f8b0cbcdd471f77d425a3a0d
Carlos Almeida <carlos.eduardo.4lmeida@gmail.com>
2026-06-21 15:43:54 +0100

    updated UI due to rotation not being controllable the way I tought off

--- SquarelineProject/ui/ui_events.h
+++ SquarelineProject/ui/ui_events.h
@@ -12,8 +12,6 @@ extern "C" {
 
 void UI_ZeroOutInclinometer(lv_event_t * e);
 void UI_UpdateBrightnessRuntime(lv_event_t * e);
-void UI_SetScreenRotation(lv_event_t * e);
-void UI_ResetScreenRotation(lv_event_t * e);
 void UI_StoreBrightness(lv_event_t * e);
 void UI_ClearNVS(lv_event_t * e);
 void UI_ConnectWifiCallback(lv_event_t * e);
